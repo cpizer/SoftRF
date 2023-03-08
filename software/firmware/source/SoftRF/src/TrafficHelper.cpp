00373e4d8dc173d4600a3c90070d9fa5b8a237f7
st148985 <st148985@stud.uni-stuttgart.de>
2023-03-08 10:09:57 +0100

    Whatever

--- software/firmware/source/SoftRF/src/TrafficHelper.cpp
+++ software/firmware/source/SoftRF/src/TrafficHelper.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "../../../advanced_options.h"
+#include "../advanced_options.h"
 #include "TrafficHelper.h"
 #include "driver/EEPROM.h"
 #include "driver/RF.h"
